f935926410f358e9ea315a49a6db3dd74afd46b8
tamjid666 <168060724+tamjid666@users.noreply.github.com>
2024-09-30 13:48:55 +0600

    add

---
+++ 01Basic.py
@@ -0,0 +1,35 @@
+import sys
+"""
+#this use for line comment
+#tripple quotes use for multiple line comment
+""" 
+
+print(sys.version)
+a = 10 
+b = 20 
+print(a+b)
+if a<b:
+    print(b," is greater than ",a)
+else:
+    print(a," is greater than ",b)
+
+#casting
+x = str(3) #x will be '3'
+y = int(3) #y will be 3
+z = float(3) #z will be 3.0
+#get the type
+print(type(x))
+print(type(y))
+print(type(z))
+
+#string enclode single or double quota no problem 
+s1 = "tamjid"
+s3 = 'sanjid'
+print(s1)
+print(s3)
+
+"""
+Camel Case Variable type : myVariableName
+Pascal Case Variable : MyVariableName
+Snake Case Variable : my_variable_name
+"""
