d222c2a4b85ec1f686b44f76085877cda80a5946
tamjid666 <168060724+tamjid666@users.noreply.github.com>
2024-09-30 13:56:02 +0600

    add

---
+++ 02Variables.py
@@ -0,0 +1,20 @@
+#many values to multiple Variables
+x,y,z = "tamjid" , "tawhid" , "Bayzid"
+print(x)
+print(y)
+print(z)
+
+print(x+" "+z)
+#one values to multiple variables
+x = y = z = "tamjid"
+print(x)
+print(y)
+print(z)
+
+print (x, " " , y)
+#unpack collection
+name = ["mahi","niky","tamjid"]
+x,y,z = name
+print(x)
+print(y)
+print(z)
