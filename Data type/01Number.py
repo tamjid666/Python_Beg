89e8464ba36fc9d1dce037af4bbfa71b30aaaf30
tamjid666 <168060724+tamjid666@users.noreply.github.com>
2024-09-30 14:30:19 +0600

    add

---
+++ Data type/01Number.py
@@ -0,0 +1,20 @@
+import random
+#int float complex
+x = 1232
+y = 3.25
+z = 3+5j
+
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#type conversion
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(a)
+print(c)
+
+print(random.randrange(1,10))
