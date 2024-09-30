141b598b716cf5f74e266b55606e4845ce1d6c9e
tamjid666 <168060724+tamjid666@users.noreply.github.com>
2024-09-30 14:45:29 +0600

    add

---
+++ 04Data type/02string.py
@@ -0,0 +1,25 @@
+a = """Abdullah Al Tamjid,Abdur Rahman Al Tawhid
+,Hamdullah Al Bayzid """
+print(a)
+
+b = '''Abdullah Al Tamjid,Abdur Rahman Al Tawhid
+,Hamdullah Al Bayzid '''
+print(b)
+
+c = "Hello,World"
+print(c[1])
+
+#loop
+for x in "banana":
+    print(x)
+print("\n")
+#string length 
+print(len(c))
+#check string
+txt = "The best things in life are free"
+print("free" in txt)
+print("tamjid" in txt)
+
+if "free" in txt:
+    print("Yes,'free' is present.")
+print("Tamjid" not in txt)
